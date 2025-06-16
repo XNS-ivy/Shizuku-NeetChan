05c36daec611db08f1c9ed16002d2a39347bc08c
XNS-ivy <ayramusic.nightcore@gmail.com>
2025-06-16 09:46:34 +0700

    rolePlay

---
+++ commands/rolePlay/neetChan.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'neet',
+    desc: 'role playing with your beloved loli',
+    premium: false,
+    execute: async ({ msg, args }) => {
+        const mongo_id = msg.id.split('@')[0]
+        const menu = args[0]
+        switch (menu) {
+            case 'sex':
+
+                break
+            case 'talk':
+
+                break
+
+            case 'stat':
+            
+                break
+            default:
+                break
+        }
+    }
+}
